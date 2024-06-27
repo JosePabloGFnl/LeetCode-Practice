3c11feea7c0503f7069139af967a70b72a5fc164
Pablo González <71510885+JosePabloGFnl@users.noreply.github.com>
2024-06-26 18:52:31 -0600

    Second attempt.

--- 2_add_two_numbers.py
+++ 2_add_two_numbers.py
@@ -20,28 +20,55 @@ Example 3:
 Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 Output: [8,9,9,9,0,0,0,1]
 '''
+from typing import Optional
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        # Function to reverse the linked list 
+        def reverse_list(head):
+            new_head = None
+            while head:
+                head.next, head, new_head = new_head, head.next, head
+            return new_head
+
+        # Function to convert linked list to int    
+        def linked_list_to_int(head: ListNode) -> int:
+            num_str = ''
+            current = head
+            while current is not None:
+                num_str += str(current.val)
+                current = current.next
+            return int(num_str)
+
+        # Function to convert int to linked list
+        def int_to_linked_list(number: int) -> ListNode:
+            dummy_head = ListNode(0)
+            current = dummy_head
+            for digit in str(number):
+                current.next = ListNode(int(digit))
+                current = current.next
+            return dummy_head.next
 
         # reverse lists
-        l1 = l1[::-1]
-        l2 = l2[::-1]
+        l1 = reverse_list(l1)
+        l2 = reverse_list(l2)
 
         # convert lists into single int number
-        l1 = int(''.join(map(str, l1)))
-        l2 = int(''.join(map(str, l2)))
+        l1 = linked_list_to_int(l1)
+        l2 = linked_list_to_int(l2)
 
         # sum both lists
         answer = l1 + l2
 
         # convert into array
-        answer = [int(x) for x in str(answer)]
+        answer = int_to_linked_list(array)
 
         # reverse once again
-        answer = answer[::-1]
+        answer = reverse_list(answer)
