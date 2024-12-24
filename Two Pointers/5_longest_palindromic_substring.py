664166f37619581f2421c69c7006ca54b8179551
Pablo González <71510885+JosePabloGFnl@users.noreply.github.com>
2024-12-24 15:05:50 -0600

    Final Answer

--- Two Pointers/5_longest_palindromic_substring.py
+++ Two Pointers/5_longest_palindromic_substring.py
@@ -24,16 +24,22 @@ Constraints:
 s consist of only digits and English letters.
 """
 class Solution:
-    def expand_from_center(s: str, left: int, right: int) -> str:
-    while left >= 0 and right < len(s) and s[left] == s[right]:
-        left -= 1
-        right += 1
-    
-    return s[left + 1:right]
-
     def longestPalindrome(self, s: str) -> str:
+        def expand_from_center(s: str, left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
 
-    if len(s) % 2 == 0:
-        expand_from_center(s, i, i + 1)
-    else:
-        expand_from_center(s, i, i)
+        longest = ""
+
+        for i in range(len(s)):
+            if len(s) % 2 == 0:
+                even = expand_from_center(s, i, i + 1)
+                if len(even) > len(longest):
+                        longest = even
+            else:
+                odd = expand_from_center(s, i, i)
+                if len(odd) > len(longest):
+                        longest = odd
+        return longest
