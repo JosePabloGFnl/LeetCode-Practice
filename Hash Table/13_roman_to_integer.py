8d2c53d72c8c8e4056a6fcbaaeefff2d7221d163
Pablo González <71510885+JosePabloGFnl@users.noreply.github.com>
2025-09-02 15:35:59 -0600

    rom_dictionary created

--- Hash Table/13_roman_to_integer.py
+++ Hash Table/13_roman_to_integer.py
@@ -43,3 +43,9 @@ Constraints:
 s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 '''
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # Roman Number Dicitonary
+        rom_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        
