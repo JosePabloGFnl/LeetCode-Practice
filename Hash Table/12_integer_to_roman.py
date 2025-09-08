12433b3286f965c89beb6c9f177f8a011448f53a
Pablo González <71510885+JosePabloGFnl@users.noreply.github.com>
2025-09-08 11:33:35 -0600

    'rom_dicitonary' created

--- Hash Table/12_integer_to_roman.py
+++ Hash Table/12_integer_to_roman.py
@@ -59,3 +59,8 @@ Constraints:
 
 1 <= num <= 3999
 '''
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        # Roman Number dicitonary
+        rom_dictionary = {'I': 1, 'IV': 5, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
+        
