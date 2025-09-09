3d087951031db1d14da5e0b697d42a424aebc902
Pablo González <71510885+JosePabloGFnl@users.noreply.github.com>
2025-09-09 11:26:58 -0600

    for loop logic

--- Hash Table/12_integer_to_roman.py
+++ Hash Table/12_integer_to_roman.py
@@ -62,5 +62,10 @@ Constraints:
 class Solution:
     def intToRoman(self, num: int) -> str:
         # Roman Number dicitonary
-        rom_dictionary = {'I': 1, 'IV': 5, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
-        
+        rom_dictionary = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
+        result = ""
+
+        # Iterate over numbers in dictionary in reverse
+        for rom_sym, int_num in (reversed(list(rom_dictionary.items()))):
+
+            
